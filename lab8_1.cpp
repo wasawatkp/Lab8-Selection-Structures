728e044f0ed8fef585c97bc6a0a6230220fbced5
wasawatkp <wasawat.upper@gmail.com>
2024-12-06 23:55:19 +0700

    lab8_1.cpp completed

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main()
+/*int main()
 {
   char rank;
   cout << "Input your rank: ";
@@ -14,4 +15,43 @@ int main()
     case 'D': cout << "You have received very KAK items.\n";
   }
   return 0;
+}*/
+
+int main()
+{
+  char rank;
+  cout << "Input your rank: ";
+  cin >> rank;
+  if(rank == 'S'){
+    cout << "You have received Super Ultra Rare Unit!!!\n";
+    cout << "You have received 5 gems.\n";
+    cout << "You have received 1 gems.\n";
+    cout << "You have received 2000 coins.\n";
+    cout << "You have received very KAK items.\n";
+    
+  }
+  else if(rank == 'A'){
+    cout << "You have received 5 gems.\n";
+    cout << "You have received 1 gems.\n";
+    cout << "You have received 2000 coins.\n";
+    cout << "You have received very KAK items.\n";
+
+  }
+  else if(rank == 'B'){
+    cout << "You have received 1 gems.\n";
+    cout << "You have received 2000 coins.\n";
+    cout << "You have received very KAK items.\n";
+
+  }
+  else if(rank == 'C'){
+    cout << "You have received 2000 coins.\n";
+    cout << "You have received very KAK items.\n";
+
+  }
+  else if(rank == 'D'){
+    cout << "You have received very KAK items.\n";
+
+  }
+  return 0;
 }
+
